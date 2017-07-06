e965249d742eb2f8be83001c9c06eb05bddecd3c
Lisa Gee <ZhuSuFeng@gmail.com>
2017-07-06 11:09:25 -0700

    Update func shared Stack constructor + stackMethods

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    storage: {},
+    counter: 0
+  };
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+
+var stackMethods = {
+  // add push
+  push: function (value) {
+    this.storage[this.counter] = value;
+    this.counter++;
+  },
+
+  // add pop
+  // decrease counter
+  // delete object
+  // return object being popped off the top
+  // add size
+  size: function() {
+    // return object.keys...
+    return Object.keys(this.storage).length;
+  }
 
 
+};
