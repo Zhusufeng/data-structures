c621d9c4a0f0b4d1872aa2a2302edda7efc20672
Lisa Gee <ZhuSuFeng@gmail.com>
2017-07-06 10:25:56 -0700

    Pass func queue size method

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,24 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  // indexToRemove = 1
+  // size
+
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    //index to add
+    //increase size
   };
 
   someInstance.dequeue = function() {
+    // decrease size
+    // increase indextoRemove
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
